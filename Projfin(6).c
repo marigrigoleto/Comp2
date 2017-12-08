9bf6311c4d9c291052f6263662ba44f5ba0905bc
marigrigoleto <34279939+marigrigoleto@users.noreply.github.com>
2017-12-08 21:35:47 -0200

    Add files via upload

--- Projfin(6).c
+++ Projfin(6).c
@@ -1,5 +1,4 @@
 /*======================================================================================================================
-
 UTFPr Engenharia Mecânica
 Mariana Mayer Grigoleto
 RA: 1907751
@@ -24,8 +23,8 @@ typedef struct produto
 } Produtos;
 
 
-Produtos* inserir_ordenado(Produtos *produtos);
-Produtos* inserir(Produtos* produtos, Produtos *novo);
+Produtos* inserir_ordenado(Produtos *produtos, Produtos* novo);
+Produtos* inserir(Produtos* produtos);
 void mostrar_produtos(Produtos* produtos);
 Produtos* abrir(Produtos* produtos);
 void busca(Produtos *produtos, char busca[20]);
@@ -35,6 +34,7 @@ void max(Produtos *produtos);
 main()
 {
     int opcao;
+    int flag = 0;
     char buscado[20];
     float gastos;
     Produtos *produtos = NULL;
@@ -47,41 +47,53 @@ main()
         switch(opcao)
         {
         case 1:
-            produtos = abrir(produtos);
-            produtos = inserir_ordenado(produtos);
+            if(flag == 0){
+                produtos = abrir(produtos);   //flag para evitar duplicação de dados caso o usuário acesse arquivos e depois decida inserir novos itens//
+                produtos = inserir(produtos);
+                }else{
+                produtos = inserir(produtos);
+                }
             break;
         case 2:
             produtos = abrir(produtos);
+            flag = 1;
             break;
         case 3:
             mostrar_produtos(produtos);
             break;
         case 4:
-            printf("\n\tElemento a ser buscado:");scanfstr(buscado);
-             int i, a = strlen(buscado);
-             for(i = 0; i < a; i++){
-             buscado[i] = toupper(buscado[i]);
-             }
-             busca(produtos,buscado);
+            printf("\n\tElemento a ser buscado:");
+            scanfstr(buscado);
+            int i, a = strlen(buscado);
+            for(i = 0; i < a; i++)
+            {
+                buscado[i] = toupper(buscado[i]);  //transforma todas os caracteres para caixa alta//
+            }
+            busca(produtos,buscado);
             break;
         case 5:
             max(produtos);
             break;
         case 6:
-           gastos = gastos_mensais(produtos);
-           if(gastos == 0){
-               printf("\n\t---NAO HA GASTOS MENSAL---\n\t");
-               }else{
-               printf("\n\tGASTO MENSAL-> %4.2f\n", gastos);
-               }
-           break;
-         }
+            gastos = gastos_mensais(produtos);
+            if(gastos == 0)
+            {
+                printf("\n\t---NAO HA GASTOS MENSAL---\n\t");
+            }
+            else
+            {
+                printf("\n\tGASTO MENSAL-> %4.2f\n", gastos);
+            }
+            break;
+        }
 
-    }while(opcao!=0);
+    }
+    while(opcao!=0);
 
 }
 
-Produtos * inserir (Produtos *produtos, Produtos *novo) {
+Produtos * inserir_ordenado (Produtos *produtos, Produtos *novo)
+{
 
     Produtos *anterior;
     Produtos *atual;
@@ -99,17 +111,17 @@ Produtos * inserir (Produtos *produtos, Produtos *novo) {
     {
         while(atual != NULL && strcmp(atual->nome, novo->nome) < 0)
         {
-            anterior = atual;
+            anterior = atual;   //caminha na lista até encontrar a posição de ordenamento (salva o atual e o anterior//
             atual = atual->prox;
         }
-        novo->prox = atual;
-        if(anterior == NULL)
+        novo->prox = atual;  //ao encontrar a posição atribui ao novo prox o fim da lista//
+        if(anterior == NULL) //analisa como ordenar o resto da lista//
         {
-            produtos = novo;
+            produtos = novo; //caso seja no inicio da lista//
         }
         else
         {
-            anterior->prox = novo;
+            anterior->prox = novo;  //caso seja no meio ou no final da lista//
         }
     }
 
@@ -117,7 +129,7 @@ Produtos * inserir (Produtos *produtos, Produtos *novo) {
 }
 
 
-Produtos* inserir_ordenado(Produtos *produtos)
+Produtos* inserir(Produtos *produtos)
 {
     char name[80];
     Produtos *novo =  (Produtos *) malloc(sizeof(Produtos));
@@ -125,9 +137,10 @@ Produtos* inserir_ordenado(Produtos *produtos)
     printf("\n\tDescricao do gasto:");
     scanfstr(name);
     int i, a = strlen(name);
-    for(i = 0; i < a; i++){
+    for(i = 0; i < a; i++)
+    {
         name[i] = toupper(name[i]);
-        }
+    }
     printf("\n\tPreco:");
     scanf("%f",&novo->preco);
     printf("\n\tQuantidade:");
@@ -136,14 +149,7 @@ Produtos* inserir_ordenado(Produtos *produtos)
     scanf("%d",&novo->mes);
     strcpy(novo->nome, name);
 
-    produtos = inserir(produtos, novo);
-
-    FILE *arq = fopen("PLANNER.txt", "a");
-    fprintf(arq, "%s\n", novo->nome);
-    fprintf(arq, "%4.2f\n", novo->preco);
-    fprintf(arq, "%d\n", novo->quantidade);
-    fprintf(arq, "%d\n", novo->mes);
-    fclose(arq);
+    produtos = inserir_ordenado(produtos, novo);
 
     return produtos;
 }
@@ -179,7 +185,7 @@ Produtos* abrir(Produtos* produtos)
         fscanf(arq,"%f\n", &novo->preco);
         fscanf(arq,"%d\n", &novo->quantidade);
         fscanf(arq,"%d\n", &novo->mes);
-        produtos = inserir(produtos, novo);
+        produtos = inserir_ordenado(produtos, novo);
         a=fscanf(arq, "%[^\n]", &nome);
     }
     fclose(arq);
@@ -188,62 +194,77 @@ Produtos* abrir(Produtos* produtos)
 
 
 void busca(Produtos *produtos, char busca[20])
-{int a = 0;
+{
+    int a = 0;
 
 
-while(produtos != NULL)
+    while(produtos != NULL)
         if(strcmp(produtos->nome, busca) == 0)
-          {printf("\n\t---Produto encontrado---\n\t");
-           printf("\n\t%s", produtos->nome);
-           printf("\n\tPRECO->%4.2f", produtos->preco);
-           printf("\n\tQNT->%d", produtos->quantidade);
-           printf("\n\tDATA->%d\n\t", produtos->mes);
-           a = 1;
-           break;
-          }else{
-          produtos = produtos->prox;
-          }
-if(a == 0)
-    printf("\n\t---Produto nao encontrado---\n\t");
+        {
+            printf("\n\t---Produto encontrado---\n\t");
+            printf("\n\t%s", produtos->nome);
+            printf("\n\tPRECO->%4.2f", produtos->preco);
+            printf("\n\tQNT->%d", produtos->quantidade);
+            printf("\n\tDATA->%d\n\t", produtos->mes);
+            a = 1;
+            break;
+        }
+        else
+        {
+            produtos = produtos->prox;
+        }
+    if(a == 0)
+        printf("\n\t---Produto nao encontrado---\n\t");
 
 }
 
 float gastos_mensais (Produtos *produtos)
 
-{int data;
-float soma = 0;
+{
+    int data;
+    float soma = 0;
 
-printf("\n\tDeseja conhecer o orcamento de qual mes: ");scanf("%d", &data);
+    printf("\n\tDeseja conhecer o orcamento de qual mes: ");
+    scanf("%d", &data);
 
-while(produtos != NULL){
-        if(produtos->mes == data){
-           soma = soma + (produtos->preco * produtos->quantidade);
-           produtos = produtos->prox;
-          }else{
-          produtos = produtos->prox;
-          }
+    while(produtos != NULL)
+    {
+        if(produtos->mes == data)
+        {
+            soma = soma + (produtos->preco * produtos->quantidade);
+            produtos = produtos->prox;
         }
+        else
+        {
+            produtos = produtos->prox;
+        }
+    }
 
-return soma;
+    return soma;
 }
 
 void max(Produtos *produtos)
-{float max = 0;
-float soma = 0;
-Produtos *maximo = NULL;
+{
+    float max = 0;
+    float soma = 0;
+    Produtos *maximo = NULL;
 
-while(produtos != NULL){
+    while(produtos != NULL)
+    {
         soma = (produtos->preco * produtos->quantidade);
-        if(soma > max){
-           max = soma;
-           maximo = produtos;
-           produtos = produtos->prox;
-          }else{
-          produtos = produtos->prox;
-          }
+        if(soma > max)
+        {
+            max = soma;
+            maximo = produtos;
+            produtos = produtos->prox;
         }
+        else
+        {
+            produtos = produtos->prox;
+        }
+    }
 
-printf("\n\t---GASTO MAXIMO---\n\t");
-printf("\n\t%s", maximo->nome);
-printf("\n\tGasto maximo->%4.2f\n\t", max);
+    printf("\n\t---GASTO MAXIMO---\n\t");
+    printf("\n\t%s", maximo->nome);
+    printf("\n\tGasto maximo->%4.2f\n\t", max);
 }
